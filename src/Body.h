d6cedc81d938171aaa24b37a950c4d85012439fd
Shahzad Lone <shahzadlone@gmail.com>
2019-06-27 19:48:54 -0400

    Implemented basic body class.
    TODO: Probably want to get this to compile with string_view (C++17)

---
+++ src/Body.h
@@ -0,0 +1,69 @@
+#include <cmath>
+#include <sstream>
+#include <string_view>
+
+enum class Colour {
+    red,
+    blue,
+    green,
+    purple
+};
+
+class Body {
+    private :
+        static constexpr double G = 6.673e-11;   // gravitational constant
+        static constexpr double solarmass = 1.98892e30;
+
+        double rx, ry;       // holds the cartesian positions
+        double vx, vy;       // velocity components
+        double fx, fy;       // force components
+        double mass;         // mass
+        Colour colour;       // colour (for fun)
+
+    public:
+        // create and initialize a new Body
+        Body(double rx, double ry, double vx, double vy, double mass, Colour colour)
+            : rx {rx} , ry {ry} , vx {vx} , vy {vy} , mass {mass} , colour{colour} { }
+
+        // update the velocity and position using a timestep dt
+        void Update(double dt) {
+            vx += dt * fx / mass;
+            vy += dt * fy / mass;
+            rx += dt * vx;
+            ry += dt * vy;
+        }
+
+        // returns the distance between two bodies
+        double DistanceTo(Body b) {
+            double dx = rx - b.rx;
+            double dy = ry - b.ry;
+            return sqrt(dx*dx + dy*dy);
+        }
+
+        // set the force to 0 for the next iteration
+        void ResetForce() {
+            fx = 0.0;
+            fy = 0.0;
+        }
+
+        // compute the net force acting between the body a and b, and
+        // add to the net force acting on a
+        void AddForce(const Body & b) {
+            double EPS = 3E4;      // softening parameter (just to avoid infinities)
+            double dx = b.rx - rx;
+            double dy = b.ry - ry;
+            double dist = sqrt(dx*dx + dy*dy);
+            double F = (G * mass * b.mass) / (dist*dist + EPS*EPS);
+            fx += F * dx / dist;
+            fy += F * dy / dist;
+        }
+
+        // convert to string representation formatted nicely
+        std::string_view ToString() {
+            return  std::to_string(rx) + ", " +
+                    std::to_string(ry) + ", " +
+                    std::to_string(vx) + ", " +
+                    std::to_string(vy) + ", " +
+                    std::to_string(mass);
+        }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "Body.h"
 
 int main() {
-    std::cout << "The Gamer Word" << std::endl;
+
+    std::cout << "The Gamer Word of Bodies Begins" << std::endl;
+
+    Body a = Body(23, 22, 3, 3, 2, Colour::green);
+
+    Body b = Body(23.2323, 23.2323, 23.2323, 23.2323, 23.2323, Colour::blue);
+
+    b.AddForce(a);
+
+    std::cout << "a : " << a.ToString() << std::endl;
+    std::cout << "b : " << b.ToString() << std::endl;
+
 }
