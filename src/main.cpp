b2514f2bc56970e564d625f417c345ef57062184
Deep Dhillon <deep@deepdhillon.ca>
2019-06-27 19:53:05 -0400

    created project more inline with qt editor

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int main() {
-    std::cout << "The Gamer Word" << std::endl;
+int main(int argc, char *argv[])
+{
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
 }
