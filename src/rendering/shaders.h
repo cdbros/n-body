1f794cfdc8bf1d4d0c84e7fa31ccde4fc3ebbec7
Mogball <jeffniu22@gmail.com>
2019-07-11 19:12:48 -0400

    alpha blending

--- src/rendering/shaders.h
+++ src/rendering/shaders.h
@@ -3,17 +3,34 @@
 
 #include <QString>
 
-const QString vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main() {\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}";
-const QString fragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main() {\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}";
+const QString vertexShaderSource = R"glsl(
+#version 410 core
+
+layout (location = 0) in vec2 pos;
+layout (location = 1) in vec2 offset;
+
+uniform mat4 mvp;
+out vec2 vpos;
+
+void main() {
+    vpos = pos;
+    gl_Position = mvp * vec4(pos + offset, 0.0f, 1.0);
+}
+)glsl";
+
+const QString fragmentShaderSource = R"glsl(
+#version 410 core
+
+out vec4 frag_color;
+in vec2 vpos;
+
+void main() {
+    float dis = sqrt(dot(vpos, vpos));
+    dis = 0.5f - dis;
+    dis = max(dis, 0.0f);
+    dis = ceil(dis / 0.5f);
+    frag_color = vec4(1.0f, 1.0f, 1.0f, dis);
+}
+)glsl";
 
 #endif // RENDERING_SHADERS_H
