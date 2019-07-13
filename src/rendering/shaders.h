a950e55332c3ad13607a667c41dd12f5925e16d1
Mogball <jeffniu22@gmail.com>
2019-07-13 11:28:04 -0400

    fixing alpha blending with depth test

--- src/rendering/shaders.h
+++ src/rendering/shaders.h
@@ -7,14 +7,14 @@ const QString vertexShaderSource = R"glsl(
 #version 410 core
 
 layout (location = 0) in vec2 pos;
-layout (location = 1) in vec2 offset;
+layout (location = 1) in vec3 offset;
 
 uniform mat4 mvp;
 out vec2 vpos;
 
 void main() {
     vpos = pos;
-    gl_Position = mvp * vec4(pos + offset, 0.0f, 1.0);
+    gl_Position = mvp * vec4(pos.x + offset.x, pos.y + offset.y, offset.z, 1.0);
 }
 )glsl";
 
