aafa4342af8f90d1eee8fbb78a42ec50734a4440
Mogball <jeffniu22@gmail.com>
2019-07-13 15:52:09 -0400

    clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,23 @@
 #include "sim_view.h"
 
 int main(int argc, char *argv[]) {
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-  // set OpenGL version to 4.1
-  QSurfaceFormat glFormat;
-  glFormat.setVersion(4, 1);
-  glFormat.setProfile(QSurfaceFormat::CoreProfile);
-  QSurfaceFormat::setDefaultFormat(glFormat);
+    // set OpenGL version to 4.1
+    QSurfaceFormat glFormat;
+    glFormat.setVersion(4, 1);
+    glFormat.setProfile(QSurfaceFormat::CoreProfile);
+    QSurfaceFormat::setDefaultFormat(glFormat);
 
-  QGuiApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
 
-  qmlRegisterType<SimView>("NBody", 1, 0, "SimView");
+    qmlRegisterType<SimView>("NBody", 1, 0, "SimView");
 
-  QQmlApplicationEngine engine;
-  engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-  if (engine.rootObjects().isEmpty()) {
-    return -1;
-  }
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty()) {
+        return -1;
+    }
 
-  return QCoreApplication::exec();
+    return QCoreApplication::exec();
 }
--- src/sim_view.cpp
+++ src/sim_view.cpp
@@ -3,35 +3,29 @@
 #include <QtQuick/qquickwindow.h>
 #include <memory>
 
-SimView::SimView() {
-  connect(this, &QQuickItem::windowChanged, this,
-          &SimView::handleWindowChanged);
-}
+SimView::SimView() { connect(this, &QQuickItem::windowChanged, this, &SimView::handleWindowChanged); }
 
 void SimView::sync() {
-  if (!m_renderer) {
-    m_renderer = std::make_unique<Renderer>();
-    connect(window(), &QQuickWindow::afterRendering, m_renderer.get(),
-            &Renderer::render, Qt::DirectConnection);
-  }
-  updateViewportSize();
-  m_renderer->setWindow(window());
+    if (!m_renderer) {
+        m_renderer = std::make_unique<Renderer>();
+        connect(window(), &QQuickWindow::afterRendering, m_renderer.get(), &Renderer::render, Qt::DirectConnection);
+    }
+    updateViewportSize();
+    m_renderer->setWindow(window());
 }
 
 void SimView::cleanup() {
-  if (m_renderer) {
-    m_renderer = nullptr;
-  }
+    if (m_renderer) {
+        m_renderer = nullptr;
+    }
 }
 
 void SimView::handleWindowChanged(QQuickWindow *window) const {
-  if (window) {
-    connect(window, &QQuickWindow::beforeSynchronizing, this, &SimView::sync,
-            Qt::DirectConnection);
-    connect(window, &QQuickWindow::sceneGraphInvalidated, this,
-            &SimView::cleanup, Qt::DirectConnection);
+    if (window) {
+        connect(window, &QQuickWindow::beforeSynchronizing, this, &SimView::sync, Qt::DirectConnection);
+        connect(window, &QQuickWindow::sceneGraphInvalidated, this, &SimView::cleanup, Qt::DirectConnection);
 
-    // prevents QML from clearing
-    window->setClearBeforeRendering(false);
-  }
+        // prevents QML from clearing
+        window->setClearBeforeRendering(false);
+    }
 }
