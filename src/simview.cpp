fa5fc5442c28c91f9abbbf81e1ccbe7b8058bb79
Deep Dhillon <deep@dhillon.io>
2019-07-14 17:56:49 -0400

    opengl working

--- src/simview.cpp
+++ src/simview.cpp
@@ -4,12 +4,9 @@ SimView::SimView(QWidget *parent) : QOpenGLWidget(parent) {}
 
 void SimView::initializeGL() {
     initializeOpenGLFunctions();
-    glClearColor(1, 0, 0, 1);
+    glClearColor(1.0, 0.0, 0.0, 1.0);
 }
 
-void SimView::paintGL() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    printf("xd\n");
-}
+void SimView::paintGL() { glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); }
 
 void SimView::resizeGL(int width, int height) { QOpenGLWidget::resizeGL(width, height); }
