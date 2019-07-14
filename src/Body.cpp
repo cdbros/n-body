28c9ff4e823e221f9fc2b74def3e6625e5b4dbeb
Shahzad Lone <shahzadlone@gmail.com>
2019-07-14 17:33:29 -0400

    Split Body.h into Body.cpp and Body.h.

---
+++ src/Body.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <sstream>
+#include <string_view>
+
+#include "Body.h"
+
+Body::Body(double rx, double ry, double vx, double vy, double mass, Colour colour)
+    : rx {rx} , ry {ry} , vx {vx} , vy {vy} , mass {mass} , colour{colour} {}
+
+    // update the velocity and position using a timestep dt
+    void Body::Update(double dt) {
+        vx += dt * (fx / mass);
+        vy += dt * (fy / mass);
+        rx += dt * vx;
+        ry += dt * vy;
+    }
+
+double Body::DistanceTo(Body b) {
+    double dx = rx - b.rx;
+    double dy = ry - b.ry;
+    return sqrt((dx * dx) + (dy * dy));
+}
+
+void Body::ResetForce() {
+    fx = 0.0;
+    fy = 0.0;
+}
+
+void Body::AddForce(const Body & b) {
+    // softening parameter (to avoid infinities).
+    double EPS = 3E4;
+
+    double dx = b.rx - rx;
+    double dy = b.ry - ry;
+
+    double dist = sqrt((dx * dx) + (dy * dy));
+    double F = ((G * mass) * b.mass) / ((dist * dist) + (EPS * EPS));
+
+    fx += F * (dx / dist);
+    fy += F * (dy / dist);
+}
+
+std::string_view Body::ToString() {
+    return  std::to_string(rx) + ", " +
+            std::to_string(ry) + ", " +
+            std::to_string(vx) + ", " +
+            std::to_string(vy) + ", " +
+            std::to_string(mass);
+}
+
--- src/Body.h
+++ src/Body.h
@@ -1,5 +1,3 @@
-#include <cmath>
-#include <sstream>
 #include <string_view>
 
 enum class Colour {
@@ -10,60 +8,43 @@ enum class Colour {
 };
 
 class Body {
+
+    public:
+        /// Constructor to Create and Initialize a new Body.
+        Body(double rx, double ry, double vx, double vy, double mass, Colour colour);
+
+        /// update the velocity and position using a timestep dt
+        void Update(double dt);
+
+        /// returns the distance between two bodies
+        double DistanceTo(Body b);
+
+        /// Set the force to 0 for the next iteration
+        void ResetForce();
+
+        /// compute the net force acting between the body a and b, and
+        /// add to the net force acting on a
+        void AddForce(const Body & b);
+
+        /// Convert to string format.
+        std::string_view ToString();
+
     private :
+
         static constexpr double G = 6.673e-11;   // gravitational constant
         static constexpr double solarmass = 1.98892e30;
 
-        double rx, ry;       // holds the cartesian positions
-        double vx, vy;       // velocity components
-        double fx, fy;       // force components
-        double mass;         // mass
-        Colour colour;       // colour (for fun)
+        /// holds the cartesian positions
+        double rx, ry;
 
-    public:
-        // create and initialize a new Body
-        Body(double rx, double ry, double vx, double vy, double mass, Colour colour)
-            : rx {rx} , ry {ry} , vx {vx} , vy {vy} , mass {mass} , colour{colour} { }
+        /// velocity components
+        double vx, vy;
 
-        // update the velocity and position using a timestep dt
-        void Update(double dt) {
-            vx += dt * fx / mass;
-            vy += dt * fy / mass;
-            rx += dt * vx;
-            ry += dt * vy;
-        }
+        /// force components
+        double fx, fy;
 
-        // returns the distance between two bodies
-        double DistanceTo(Body b) {
-            double dx = rx - b.rx;
-            double dy = ry - b.ry;
-            return sqrt(dx*dx + dy*dy);
-        }
+        double mass;
 
-        // set the force to 0 for the next iteration
-        void ResetForce() {
-            fx = 0.0;
-            fy = 0.0;
-        }
+        Colour colour;
 
-        // compute the net force acting between the body a and b, and
-        // add to the net force acting on a
-        void AddForce(const Body & b) {
-            double EPS = 3E4;      // softening parameter (just to avoid infinities)
-            double dx = b.rx - rx;
-            double dy = b.ry - ry;
-            double dist = sqrt((dx * dx) + (dy * dy));
-            double F = ((G * mass) * b.mass) / ((dist * dist) + (EPS * EPS));
-            fx += F * dx / dist;
-            fy += F * dy / dist;
-        }
-
-        // convert to string representation formatted nicely
-        std::string_view ToString() {
-            return  std::to_string(rx) + ", " +
-                    std::to_string(ry) + ", " +
-                    std::to_string(vx) + ", " +
-                    std::to_string(vy) + ", " +
-                    std::to_string(mass);
-        }
 };
