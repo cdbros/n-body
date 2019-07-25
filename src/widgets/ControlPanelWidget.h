d6de1e0767e22cb6d1757aaddcfdc9605c291893
Deep Dhillon <deep@deepdhillon.ca>
2019-07-25 00:48:21 -0400

    widget creation memory fixes

--- src/widgets/ControlPanelWidget.h
+++ src/widgets/ControlPanelWidget.h
@@ -11,17 +11,6 @@ class ControlPanelWidget : public QWidget {
 
   public:
     explicit ControlPanelWidget(QWidget *parent = nullptr);
-
-  private:
-    std::unique_ptr<QVBoxLayout> m_vLayout;
-    std::unique_ptr<Section> m_section1;
-    std::unique_ptr<QVBoxLayout> m_section1Layout;
-    std::unique_ptr<Section> m_section2;
-    std::unique_ptr<QVBoxLayout> m_section2Layout;
-    std::unique_ptr<Section> m_section3;
-    std::unique_ptr<QVBoxLayout> m_section3Layout;
-    std::unique_ptr<Section> m_section4;
-    std::unique_ptr<QVBoxLayout> m_section4Layout;
 };
 
 #endif // CONTROLPANELWIDGET_H
